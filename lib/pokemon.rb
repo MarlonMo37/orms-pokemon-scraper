e7426847ac091901b58894ec5d10bc84ce786da0
MarlonMo37 <marlonmoreno894@gmail.com>
2020-12-23 22:42:52 -0600

    commit tree c87f5ceebae0054590c177c3be9e4c3c89daebd1

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,23 @@
+require "pry"
 class Pokemon
+
+    attr_accessor :name, :type, :db, :id
+   
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    end
+
+    def self.find(id_num, db)
+        poke_info = db.execute("SELECT * FROM pokemon WHERE id = ?", id_num).flatten
+        Pokemon.new(id: poke_info[0], name: poke_info[1], type: poke_info[2], db: db)
+    end
+
+
 end
